5ff07e7092552cba350e938463cde3478e2e52b0
onticinc <onticinc@gmail.com>
2023-03-02 00:27:54 -0700

    finished basic types section 2

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('your code goes here...');
+// add :number to the function declaration to make it explicit that the function returns a number. 
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 === 'number' || typeof n2 === 'number'){
+    //     throw new Error('Incorrect input!');
+    const result = n1 + n2
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 1;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is:';
+
+add(number1, number2, printResult, resultPhrase);
--- section-2/readme.md
+++ section-2/readme.md
@@ -1,4 +1,4 @@
-Core Types
+Typscript Types
 
 # number
 All Numbers, no differentiation between integers or floats
@@ -11,4 +11,7 @@ All Text Values
 True False
 Just the two, no "truthy" or "falsy" values
 
-#
+# Object
+{age:30} 
+Any Javascript object, more specific types (type of object) are possible. 
+
