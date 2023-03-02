ab89c3a526324e2135ead6fb1a17d2d2f0d654e6
onticinc <onticinc@gmail.com>
2023-03-02 01:09:46 -0700

    covered enum, and any types

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-// add :number to the function declaration to make it explicit that the function returns a number. 
-function add(n1, n2, showResult, phrase) {
-    // if (typeof n1 === 'number' || typeof n2 === 'number'){
-    //     throw new Error('Incorrect input!');
-    const result = n1 + n2
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+; // Enum = Assigns labels to number values. 
+// ### Object Types & Key "Type" Pairs (Key Value Pairs)
+// ### Using a tuple to define the role property of the person object.
+// 
+// const person: {
+//     name: string;
+//     age: number;
+//     nickname?: string; // ? makes the property optional
+//     hobbies: string[]; // Array
+//     role: [number, string]; // Tuple
+// } = {
+//     name: "Maximilian",
+//     age: 30,
+//     nickname: "Max",
+//     hobbies: ["Sports", "Cooking"],
+//     role: [2, "author"]
+// };
+// Object Types & Key "Type" Pairs (Key Value Pairs)
+var person = {
+    name: "Maximilian",
+    age: 30,
+    nickname: "Max",
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
+};
+var favoriteActivities;
+console.log(person.nickname);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) // !!! ERROR !!!
 }
-var number1 = 1;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is:';
-
-add(number1, number2, printResult, resultPhrase);
--- section-2/readme.md
+++ section-2/readme.md
@@ -4,7 +4,6 @@ Typscript Types
 All Numbers, no differentiation between integers or floats
 
 # string
-
 All Text Values
 
 # boolean
@@ -14,4 +13,15 @@ Just the two, no "truthy" or "falsy" values
 # Object
 {age:30} 
 Any Javascript object, more specific types (type of object) are possible. 
+
+# Array 
+[1,2.3,]
+Any JavaScript array, type can be flexible or strict(regarding the element types)
+
+# ENUM
+enum (NEW, OLD)
+Added by TypesScript: Automaticall enumerated global constant identifiers.
+
+# Any Type *
+Lets anything go. Should be avoided because it takes away the advantages of TS. 
 
